d427fbf653abcb9c7f4f7761eb141eaf2b9e5041
Joaquim <oliver-junior@hotmail.com>
2024-07-01 19:38:27 -0300

    Initial commit

---
+++ tupple.py
@@ -0,0 +1,46 @@
+# thistuple = ('apple', 'banana','cherry', 'apple', 'cherry')
+# print(thistuple)
+
+# thistuple = ('apple', 'banana','cherry')
+# print(len(thistuple))
+
+# thistuple = ('apple',)
+# print(type(thistuple))
+
+# thistuple = ('apple')
+# print(type(thistuple))
+
+# thistuple = ('apple', 'banana','cherry')
+# y = list(thistuple)
+# y.append('orange')
+# thistuple = tuple(y)
+# print(thistuple)
+
+# thistuple = ('apple', 'banana','cherry')
+# (green, yellow, red) = thistuple
+# print(green)
+# print(yellow)
+# print(red)
+
+# thistuple = ('apple', 'banana','cherry', 'strawberry', 'raspberry')
+# (green, *yellow, red) = thistuple
+# print(green)
+# print(yellow)
+# print(red)
+
+# thistuple = ('apple', 'banana','cherry')
+# for x in thistuple:
+#     print(x)
+
+# thistuple = ('apple', 'banana','cherry')
+# for i in range(len(thistuple)):
+#     print(thistuple[i])
+
+# tuple1 = ('a', 'b', 'c')
+# tuple2 = (1,2,3)
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+fruits = ('apple', 'banana', 'cherry')
+mytupple = fruits*2
+print(mytupple)
