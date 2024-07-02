847aec853df968bd71bd5c21fa749fa11a4302e6
Joaquim <oliver-junior@hotmail.com>
2024-07-02 19:51:24 -0300

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,24 @@
+thisdict = {
+    'brand': 'Ford',
+    'model': 'Mustang',
+    'year': 1964,
+    'color': 'white'
+#     # 'year': 2020,
+#     # 'color': ['red', 'white', 'blue']
+}
+# # print(thisdict)
+# # # print(thisdict['brand'])
+# # print(len(thisdict))
+# print(type(thisdict))
+# thisdict = dict(name = 'John', age=36, country = 'Norway')
+# print(thisdict)
+# x = thisdict['model']
+# print(x)
+
+# x = thisdict.keys()
+# print(x)
+
+x = thisdict.keys()
+print(x)
+thisdict['color'] = 'white'
+print(thisdict)
