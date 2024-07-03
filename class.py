4b059c2bae1fecced1d674eb04a826ed88e9491f
Joaquim <oliver-junior@hotmail.com>
2024-07-03 16:40:04 -0300

    Create class.py

---
+++ class.py
@@ -0,0 +1,55 @@
+# class MyClass:
+#     x = 5
+# print(MyClass)
+
+# class MyClass:
+#     x = 5
+# p1 = MyClass()
+# print(p1.x)
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# p1 = Person('John', 36)
+# print(p1.name)
+# print(p1.age)
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def myfunc(self):
+#         print('Hello my name is '+ self.name)
+# p1 = Person('John', 36)
+# p1.myfunc()
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def myfunc(self):
+#         print('Hello my name is ' + self.name)
+# p1 = Person('John', 36)
+# p1.age = 40
+# print(p1.age)
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def myfunc(self):
+#         print('Hello my name is ' + self.name)
+# p1 = Person('John', 36)
+# del p1.age
+# print(p1.age)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def myfunc(self):
+        print('Hello my name is ' + self.name)
+p1 = Person('John', 36)
+del p1
+print(p1.age)
