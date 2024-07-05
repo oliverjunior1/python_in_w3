81552251bebe9fdc5fe2e364ca9ab0e8ce32c013
Joaquim <oliver-junior@hotmail.com>
2024-07-04 23:49:22 -0300

    class and objects, and iterators

--- class_and_objects.py
+++ class_and_objects.py
@@ -17,11 +17,11 @@
 # p1.age = 40
 # print(p1.age)
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# p1 = Person('John', 36)
+# print(p1.name)
+# print(p1.age)
 
-p1 = Person('John', 36)
-print(p1.name)
-print(p1.age)
--- iterators.py
+++ iterators.py
@@ -33,18 +33,19 @@
 # print(next(myiter))
 # print(next(myiter))
 
-class MyNumbers:
-    def __iter__(self):
-        self.a = 1
-        return self
-    def __next__(self):
-        if self.a <= 20:
-            x = self.a
-            self.a += 1
-            return x
-        else:
-            raise StopIteration
-myclass = MyNumbers()
-myiter = iter(myclass)
-for x in myiter:
-    print(x)
+# class MyNumbers:
+#     def __iter__(self):
+#         self.a = 1
+#         return self
+#     def __next__(self):
+#         if self.a <= 20:
+#             x = self.a
+#             self.a += 1
+#             return x
+#         else:
+#             raise StopIteration
+# myclass = MyNumbers()
+# myiter = iter(myclass)
+# for x in myiter:
+#     print(x)
+
