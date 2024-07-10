63688fdef7a645291a9d1c1b042e24d83a04d215
Joaquim <oliver-junior@hotmail.com>
2024-07-09 23:22:57 -0300

    file_write added

---
+++ file_write.py
@@ -0,0 +1,14 @@
+# f = open('demolife2.txt', 'a')
+# f.write('Now the file has more content!')
+# f.close()
+
+# f = open('demolife2.txt', 'r')
+# print(f.read())
+###############################################
+f = open('C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile2.txt', 'a')
+f.write('Now the file has more content!')
+f.close()
+
+f = open('C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile2.txt', 'r')
+print(f.read())
+###############################################
