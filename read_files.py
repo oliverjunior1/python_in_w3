2970c77d939bb555df2c3964de93df237a709d0b
Joaquim <oliver-junior@hotmail.com>
2024-07-09 23:02:55 -0300

    Update read_files.py

--- read_files.py
+++ read_files.py
@@ -1,2 +1,22 @@
-f = open("demofile.txt", "r")
-print(f.read())
+# f = open("C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile.txt", "r")
+# print(f.read())
+
+############################################################################
+# f = open("C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile.txt", "r")
+# print(f.read(5))
+
+############################################################################
+# f = open("C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile.txt", "r")
+# print(f.readline())
+############################################################################
+# f = open("C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile.txt", "r")
+# print(f.readline())
+# print(f.readline())
+############################################################################
+# f = open("C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile.txt", "r")
+# for x in f:
+#     print(x)
+############################################################################
+f = open("C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile.txt", "r")
+print(f.readline())
+f.close()
