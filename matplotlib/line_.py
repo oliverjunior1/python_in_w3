d3e5c74a731577ede881b00c09e28d475ad27ae6
Joaquim <oliver-junior@hotmail.com>
2024-07-10 17:25:02 -0300

    Create line_.py

---
+++ matplotlib/line_.py
@@ -0,0 +1,86 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, linestyle = 'dotted')
+# plt.show()
+
+#########################################
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y= np.array([3,4,8,5])
+
+# plt.plot(y, linestyle = 'dashed')
+# plt.show()
+#########################################
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([4,5,10,8])
+
+# plt.plot(y, ls= ':')
+# plt.show()
+
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, color='r')
+# plt.show()
+
+#########################################
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, color='#4CAF50')
+# plt.show()
+
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = ([10,2,8,0])
+
+# plt.plot(y, color='pink')
+# plt.show()
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, linewidth='20.5')
+# plt.show()
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+# y2 = np.array([6,2,7,11])
+
+# plt.plot(y2)
+# plt.plot(y)
+
+# plt.show()
+#########################################
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = np.array([0,1,2,3])
+y1 = np.array([3,8,1,10])
+x2 = np.array([0,1,2,3])
+y2 = np.array([6,2,7,11])
+
+plt.plot(x1, y1, x2, y2)
+
+plt.show()
+#########################################
