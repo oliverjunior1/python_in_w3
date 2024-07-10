e89a53d6ff402899508640faf7e9ab961be96c19
Joaquim <oliver-junior@hotmail.com>
2024-07-09 23:38:45 -0300

    delete_file added

---
+++ delete_file.py
@@ -0,0 +1,9 @@
+# import os
+# os.remove('C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile2.txt')
+###############################################
+import os
+if os.path.exists('C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile2.txt'):
+    os.remove('C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\demofile2.txt')
+else:
+    print('The file does not exist')
+###############################################
