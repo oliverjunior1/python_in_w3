39d2300e303cfddfb8e14dd1f5641fb80e5c62d3
Joaquim <oliver-junior@hotmail.com>
2024-07-09 23:53:50 -0300

    Create plotting.py

---
+++ matplotlib/plotting.py
@@ -0,0 +1,37 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# xpoints = np.array([1,8])
+# ypoints = np.array([3,10])
+
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
+############################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array([1,8])
+# y = np.array([3,10])
+
+# plt.plot(x, y, 'o')
+# plt.show()
+
+############################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array([1,2,6,8])
+# y = np.array([3,8,1,10])
+
+# plt.plot(x, y)
+# plt.show()
+############################################
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([3,8,1,10,5,7])
+
+plt.plot(y)
+plt.show()
+############################################
