4aa83f8926dbe51316527a8feec9c5f37ffb0d96
Joaquim <oliver-junior@hotmail.com>
2024-07-10 00:14:41 -0300

    Create markers.py

---
+++ matplotlib/markers.py
@@ -0,0 +1,79 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, marker = 'o')
+# plt.show()
+
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, marker = '*')
+# plt.show()
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, 'o:r')
+# plt.show()
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, marker='o', ms=20)
+# plt.show()
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, marker='o', ms=20, mec='r')
+# plt.show()
+
+
+
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, marker='o', ms=20, mfc='r')
+# plt.show()
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, marker='o', ms=20, mec= 'r',mfc='r')
+# plt.show()
+#########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y = np.array([3,8,1,10])
+
+# plt.plot(y, marker='o', ms=20,mec='#4CAF50', mfc='#4CAF50')
+# plt.show()
+#########################################
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([3,8,1,10])
+
+plt.plot(y, marker='o', ms=20, mfc='hotpink', mec='hotpink')
+plt.show()
+#########################################
+
+
+
