1cd71ca8a22ab3bc55aec92a8930cd18a4a10008
Joaquim <oliver-junior@hotmail.com>
2024-07-11 20:24:25 -0300

    Create histogram.py

---
+++ matplotlib/histogram.py
@@ -0,0 +1,14 @@
+# import numpy as np
+
+# x = np.random.normal(170,10,250)
+
+# print(x)
+
+###########################################
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.normal(170,10,250)
+
+plt.hist(x)
+plt.show()
