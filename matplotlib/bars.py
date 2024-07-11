e5b232f24733bb2ad1a559e4643b0919f58837a5
Joaquim <oliver-junior@hotmail.com>
2024-07-11 20:20:58 -0300

    Create bars.py

---
+++ matplotlib/bars.py
@@ -0,0 +1,63 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array(['A', 'B', 'C', 'D'])
+# y = np.array([3,8,1,10])
+
+# plt.bar(x, y)
+# plt.show()
+#############################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array(['A', 'B', 'C', 'D'])
+# y = np.array([3,8,1,10])
+
+# plt.barh(x, y)
+# plt.show()
+#############################################
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array(['A', 'B', 'C', 'D'])
+# y = np.array([3,8,1,10])
+
+# plt.bar(x, y, color='red')
+# plt.show()
+#############################################
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array(['A', 'B', 'C', 'D'])
+# y = np.array([3,8,1,10])
+
+# plt.bar(x, y, color='hotpink')
+# plt.show()
+#############################################
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x= np.array(['A', 'B', 'C', 'D'])
+# y= np.array([3,8,1,10])
+
+# plt.bar(x, y, color="#4CAF50")
+# plt.show()
+#############################################
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array(['A', 'B', 'C', 'D'])
+# y = np.array([3,8,1,10])
+
+# plt.bar(x, y, width=0.1)
+# plt.show()
+#############################################
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array(['A', 'B', 'C', 'D'])
+y = np.array([3,8,1,10])
+
+plt.barh(x, y, height=0.1)
+plt.show()
+#############################################
