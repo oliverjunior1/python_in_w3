2be8d5bd62db61c7bfd555c49620d55694d48af1
Joaquim <oliver-junior@hotmail.com>
2024-07-10 22:00:20 -0300

    Create scatter.py

---
+++ matplotlib/scatter.py
@@ -0,0 +1,75 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+
+# plt.scatter(x, y)
+# plt.show()
+
+###################################
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# plt.scatter(x, y)
+
+# x = np.array([2,2,8,1,15,8,12,9,7,3,11,7,14,12,15])
+# y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+# plt.scatter(x, y)
+
+# plt.show()
+###################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# plt.scatter(x, y, color='hotpink')
+
+# x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+# y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+# plt.scatter(x, y, color='#88c999')
+
+# plt.show()
+###################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# colors = np.array(['red', 'green', 'blue', 'yellow', 'pink', 'black', 'orange', 'purple', 'beige', 'brown', 'gray', 'cyan', 'magenta'])
+# plt.scatter(x, y, c = colors)
+
+# plt.show()
+###################################
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# colors = np.array([0,10,20,30,40,45,50,55,60,70,80,90,100])
+
+# plt.scatter(x, y, c=colors, cmap='viridis')
+# plt.show()
+###################################
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+colors = np.array([0,10,20,30,40,45,50,55,60,70,80,90,100])
+
+plt.scatter(x, y, c=colors, cmap='viridis')
+
+plt.colorbar()
+
+plt.show()
+###################################
+
+###################################
+
+###################################
+
+###################################
