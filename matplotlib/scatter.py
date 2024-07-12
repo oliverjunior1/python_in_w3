cee2b1a9055abe932766f0e18e0bad7afb578cf5
Joaquim <oliver-junior@hotmail.com>
2024-07-12 19:31:58 -0300

    Update scatter.py

--- matplotlib/scatter.py
+++ matplotlib/scatter.py
@@ -89,17 +89,18 @@
 
 # plt.show()
 ###################################
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-x = np.random.randint(100, size=(100))
-y = np.random.randint(100, size=(100))
-colors = np.random.randint(100, size=(100))
-sizes = 10* np.random.randint(100, size=(100))
+# x = np.random.randint(100, size=(100))
+# y = np.random.randint(100, size=(100))
+# colors = np.random.randint(100, size=(100))
+# sizes = 10* np.random.randint(100, size=(100))
 
-plt.scatter(x, y, c = colors, s= sizes, alpha=0.5, cmap='nipy_spectral')
+# plt.scatter(x, y, c = colors, s= sizes, alpha=0.5, cmap='nipy_spectral')
 
-plt.colorbar()
+# plt.colorbar()
 
-plt.show()
+# plt.show()
 ###################################
+
