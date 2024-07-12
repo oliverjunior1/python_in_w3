b557204c1c411749177de4c3f1e20eb108681381
Joaquim <oliver-junior@hotmail.com>
2024-07-12 20:19:14 -0300

    Create array_copy_vs_view.py

---
+++ numpy/array_copy_vs_view.py
@@ -0,0 +1,37 @@
+# import numpy as np
+
+# arr = np.array([1,2,3,4,5])
+# x = arr.copy()
+# arr[0] = 42
+
+# print(arr)
+# print(x)
+####################################
+# import numpy as np
+
+# arr = np.array([1,2,3,4,5])
+# x = arr.view()
+# arr[0] = 42
+
+# print(arr)
+# print(x)
+####################################
+# import numpy as np
+
+# arr = np.array([1,2,3,4,5])
+# x = arr.view()
+# x[0] = 31
+
+# print(arr)
+# print(x)
+####################################
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+
+x = arr.copy()
+y = arr.view()
+
+print(x.base)
+print(y.base)
