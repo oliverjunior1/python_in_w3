7c1b506227bea1f2b675eece02b833fb1e7658c8
Joaquim <oliver-junior@hotmail.com>
2024-07-13 16:18:38 -0300

    Create sorting_arrays.py

---
+++ numpy/sorting_arrays.py
@@ -0,0 +1,27 @@
+# import numpy as np
+
+# arr =np.array([3,2,0,1])
+
+# print(np.sort(arr))
+##########################################
+
+# import numpy as np
+
+# arr =np.array(['banana', 'cherry', 'apple'])
+
+# print(np.sort(arr))
+
+##########################################
+
+# import numpy as np
+
+# arr =np.array([True, False, True])
+
+# print(np.sort(arr))
+##########################################
+
+import numpy as np
+
+arr =np.array([[3,2,4],[5,0,1]])
+
+print(np.sort(arr))
