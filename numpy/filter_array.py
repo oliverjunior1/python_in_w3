071b1682e8d02afe4f84871f8553a94e5bedcd6a
Joaquim <oliver-junior@hotmail.com>
2024-07-13 16:30:40 -0300

    Create filter_array.py

---
+++ numpy/filter_array.py
@@ -0,0 +1,72 @@
+# import numpy as np
+
+# arr = np.array([41,42,43,44])
+
+# x = [True, False, True, False]
+
+# newarr = arr[x]
+
+# print(newarr)
+
+##########################################
+
+# import numpy as np
+
+# arr = np.array([41,42,43,44])
+
+# filter_arr = []
+
+# for element in arr:
+#     if element > 42:
+#         filter_arr.append(True)
+#     else:
+#         filter_arr.append(False)
+
+# newarr = arr[filter_arr]
+
+# print(newarr)
+
+##########################################
+
+# import numpy as np
+
+# arr = np.array([1,2,3,4,5,6,7])
+
+# filter_arr = []
+
+# for element in arr:
+#     if element % 2 == 0:
+#         filter_arr.append(True)
+#     else:
+#         filter_arr.append(False)
+
+# newarr = arr[filter_arr]
+
+# print(newarr)
+# print(filter_arr)
+
+##########################################
+
+# import numpy as np
+
+# arr = np.array([41,42,43,44])
+
+# filter_arr = arr > 42
+
+# newarr = arr[filter_arr]
+
+# print(newarr)
+# print(filter_arr)
+
+##########################################
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6,7])
+
+filter_arr = arr % 2==0
+
+newarr = arr[filter_arr]
+
+print(newarr)
+print(filter_arr)
