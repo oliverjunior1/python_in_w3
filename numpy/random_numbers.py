945fad3bcf54d1a08c1798c7beae6aac3ce5d4fb
Joaquim <oliver-junior@hotmail.com>
2024-07-13 17:40:18 -0300

    Create random_numbers.py

---
+++ numpy/random_numbers.py
@@ -0,0 +1,60 @@
+# from numpy import random
+
+# x = random.randint(100)
+
+# print(x)
+
+##########################################
+
+# from numpy import random
+
+# x = random.rand()
+
+# print(x)
+
+##########################################
+
+# from numpy import random
+
+# x = random.randint(100, size=(5))
+
+# print(x)
+
+##########################################
+
+# from numpy import random
+
+# x = random.randint(100, size=(3,5))
+
+# print(x)
+
+##########################################
+
+# from numpy import random
+
+# x = random.rand(3,5)
+
+# print(x)
+
+##########################################
+
+# from numpy import random
+
+# x = random.rand(3,5)
+
+# print(x)
+
+##########################################
+
+# from numpy import random
+
+# x = random.choice([3,5,7,9])
+
+# print(x)
+
+##########################################
+from numpy import random
+
+x = random.choice([3,5,7,9], size=(3,5))
+
+print(x)
