a6f2d85cfe265ae6400eafb9cb1b237842966320
Joaquim <oliver-junior@hotmail.com>
2024-07-12 22:03:08 -0300

    array_shape

---
+++ numpy/array_shape.py
@@ -0,0 +1,14 @@
+# import numpy as np
+
+# arr = np.array([[1,2,3,4],[5,6,7,8]])
+
+# print(arr.shape)
+
+##########################################
+
+import numpy as np
+
+arr = np.array([1,2,3,4], ndmin=5)
+
+print(arr)
+print('shape of array: ', arr.shape)
