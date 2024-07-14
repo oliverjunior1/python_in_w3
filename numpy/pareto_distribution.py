9e6a4b6a0ef97e3bc3ef1fce886b4fc84da565e7
Joaquim <oliver-junior@hotmail.com>
2024-07-14 11:54:51 -0300

    Create pareto_distribution.py

---
+++ numpy/pareto_distribution.py
@@ -0,0 +1,15 @@
+# from numpy import random
+
+# x = random.pareto(a=2, size=(2,3))
+
+# print(x)
+
+##########################################
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.pareto(a=2, size=1000), kde=False)
+
+plt.show()
