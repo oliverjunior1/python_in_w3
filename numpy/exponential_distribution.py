6d55ea499d7b2e5417f41cfcd24a1f2ef3f90fec
Joaquim <oliver-junior@hotmail.com>
2024-07-14 10:42:38 -0300

    Create exponential_distribution.py

---
+++ numpy/exponential_distribution.py
@@ -0,0 +1,15 @@
+# from numpy import random
+
+# x = random.exponential(scale=2, size=(2, 3))
+
+# print(x)
+
+##########################################
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.exponential(size=1000), hist=False)
+
+plt.show()
