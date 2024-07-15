da959cb810bd2cfb96310950f7d5b37d34e2fb06
Joaquim <oliver-junior@hotmail.com>
2024-07-15 19:32:55 -0300

    Create lcm_lowest_common_multiple.py

---
+++ numpy/lcm_lowest_common_multiple.py
@@ -0,0 +1,28 @@
+# import numpy as np
+
+# num1 = 4
+# num2 = 6
+
+# x = np.lcm(num1, num2)
+
+# print(x)
+
+###########################################
+
+# import numpy as np
+
+# arr = np.array([3,6,9])
+
+# newarr = np.lcm.reduce(arr)
+
+# print(newarr)
+
+###########################################
+
+import numpy as np
+
+arr = np.arange(1,11)
+
+newarr = np.lcm.reduce(arr)
+
+print(newarr)
