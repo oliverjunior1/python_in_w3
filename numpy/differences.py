bfeaa3555be57d8df9981e86573779ad6c875ec1
Joaquim <oliver-junior@hotmail.com>
2024-07-15 19:27:51 -0300

    Create differences.py

---
+++ numpy/differences.py
@@ -0,0 +1,17 @@
+# import numpy as np
+
+# arr = np.array([10,15,25,5])
+
+# newarr = np.diff(arr)
+
+# print(newarr)
+
+###########################################
+
+import numpy as np
+
+arr = np.array([10,15,25,5])
+
+newarr = np.diff(arr, n=2)
+
+print(newarr)
