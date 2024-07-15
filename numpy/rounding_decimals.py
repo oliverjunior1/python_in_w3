73155bed155380af32b35112c33937a0de12e98e
Joaquim <oliver-junior@hotmail.com>
2024-07-15 18:34:47 -0300

    Create rounding_decimals.py

---
+++ numpy/rounding_decimals.py
@@ -0,0 +1,27 @@
+# import numpy as np
+
+# arr = np.trunc([-3.1666, 3.6667])
+
+# print(arr)
+
+###########################################
+
+# import numpy as np
+
+# arr = np.fix([-3.1666, 3.6667])
+
+# print(arr)
+
+###########################################
+# import numpy as np
+
+# arr = np.around([-3.1666, 2])
+
+# print(arr)
+
+###########################################
+import numpy as np
+
+arr = np.ceil([-3.1666, 3.6667])
+
+print(arr)
