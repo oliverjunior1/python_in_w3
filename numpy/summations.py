4a0238cd77be0d440f68fc165b5edd726e79ed3f
Joaquim <oliver-junior@hotmail.com>
2024-07-15 19:06:11 -0300

    Create summations.py

---
+++ numpy/summations.py
@@ -0,0 +1,40 @@
+# import numpy as np
+
+# arr1 = np.array([1,2,3])
+# arr2 = np.array([1,2,3])
+
+# newarr = np.add(arr1, arr2)
+
+# print(newarr)
+###########################################
+
+# import numpy as np
+
+# arr1 = np.array([1,2,3])
+# arr2 = np.array([1,2,3])
+
+# newarr = np.sum([arr1, arr2])
+
+# print(newarr)
+
+###########################################
+
+# import numpy as np
+
+# arr1 = np.array([1,2,3])
+# arr2 = np.array([1,2,3])
+
+# newarr = np.sum([arr1, arr2], axis=1)
+
+# print(newarr)
+
+###########################################
+
+import numpy as np
+
+arr = np.array([1,2,3])
+
+
+newarr = np.cumsum(arr)
+
+print(newarr)
