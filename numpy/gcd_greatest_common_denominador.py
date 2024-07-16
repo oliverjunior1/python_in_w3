6d3dbe7c26a6ba77c581696b93e03c64d719ded4
Joaquim <oliver-junior@hotmail.com>
2024-07-16 19:05:23 -0300

    Update gcd_greatest_common_denominador.py

--- numpy/gcd_greatest_common_denominador.py
+++ numpy/gcd_greatest_common_denominador.py
@@ -9,10 +9,13 @@
 
 ###########################################
 
-import numpy as np
+# import numpy as np
 
-arr = np.array([20,8,32,36,16])
+# arr = np.array([20,8,32,36,16])
 
-x = np.gcd.reduce(arr)
+# x = np.gcd.reduce(arr)
 
-print(x)
+# print(x)
+
+###########################################
+
