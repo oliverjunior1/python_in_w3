3ea7b23ddddbe71786d8cc634e01c07bbe9e4d3b
Joaquim <oliver-junior@hotmail.com>
2024-07-17 16:27:36 -0300

    Create analyzing_dataframes.py

---
+++ pandas/analyzing_dataframes.py
@@ -0,0 +1,14 @@
+# import pandas as pd
+
+# df = pd.read_csv('C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\pandas\\data.csv')
+
+# print(df.head(10))
+
+
+##########################################
+
+import pandas as pd
+
+df = pd.read_csv('C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\pandas\\data.csv')
+
+print(df.head())
