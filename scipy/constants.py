4277216382db42fa32a62a9079285cc877de825f
Joaquim <oliver-junior@hotmail.com>
2024-07-18 19:41:24 -0300

    Create constants.py

---
+++ scipy/constants.py
@@ -0,0 +1,16 @@
+# from scipy import constants
+
+# print(constants.pi)
+
+#################################################
+
+# from scipy import constants
+
+# print(dir(constants))
+
+#################################################
+
+from scipy import constants
+
+print(constants.hectare)
+print(constants.acre)
