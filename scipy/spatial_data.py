b33971929390ec3ef1996e3e3615a6697ef3731c
Joaquim <oliver-junior@hotmail.com>
2024-07-20 16:22:14 -0300

    Create spatial_data.py

---
+++ scipy/spatial_data.py
@@ -0,0 +1,46 @@
+# import numpy as np
+# from scipy.spatial import Delaunay
+# import matplotlib.pyplot as plt
+
+# points = np.array([
+#     [2,4],
+#     [3,4],
+#     [3,0],
+#     [2,2],
+#     [4,1]
+# ])
+
+# simplices = Delaunay(points).simplices
+
+# plt.triplot(points[:,0], points[:,1], simplices)
+# plt.scatter(points[:,0], points[:,1], color='r')
+
+# plt.show()
+
+####################################################
+
+import numpy as np
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+
+points = np.array([
+    [2,4],
+    [3,4],
+    [3,0],
+    [2,2],
+    [4,1],
+    [1,2],
+    [5,0],
+    [3,1],
+    [1,2],
+    [0,2]
+])
+
+hull = ConvexHull(points)
+hull_points = hull.simplices
+
+plt.scatter(points[:,0], points[:,1])
+for simplex in hull_points:
+    plt.plot(points[simplex,0], points[simplex, 1],'k-')
+
+plt.show()
