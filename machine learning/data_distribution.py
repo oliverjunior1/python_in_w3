9c3b29f77d3b8cf701714b97c5e14aafc1ebdfbe
Joaquim <oliver-junior@hotmail.com>
2024-07-20 22:35:04 -0300

    Create data_distribution.py

---
+++ machine learning/data_distribution.py
@@ -0,0 +1,15 @@
+# import numpy
+
+# x = numpy.random.uniform(0.0,5.0,250)
+
+# print(x)
+
+#################################################
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.uniform(0.0, 5.0, 250)
+
+plt.hist(x, 5)
+plt.show()
