b32fc1da6d22919bc6b219872196550ad93025c1
Joaquim <oliver-junior@hotmail.com>
2024-07-23 19:21:56 -0300

    Update decision_tree.py

--- machine learning/decision_tree.py
+++ machine learning/decision_tree.py
@@ -1,8 +1,17 @@
-import pandas
+# import pandas
 
-df = pandas.read_csv('machine learning\\data1.csv')
+# df = pandas.read_csv('C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\machine learning\\data1.csv')
 
-print(df)
+# print(df)
 
 #################################################
+import pandas
 
+df = pandas.read_csv("C:\\Users\\Olive\\Downloads\\Visual Studio Code\\W3\\python\\machine learning\\data.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+print(df)
